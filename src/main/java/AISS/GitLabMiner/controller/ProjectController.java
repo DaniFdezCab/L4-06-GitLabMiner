761c4679d6e69a8deb12fd21547d635556b54c62
DaniFdezCab <danfercab@alum.us.es>
2023-05-13 16:32:21 +0200

    version1.1

--- src/main/java/AISS/GitLabMiner/controller/ProjectController.java
+++ src/main/java/AISS/GitLabMiner/controller/ProjectController.java
@@ -5,55 +5,51 @@ import AISS.GitLabMiner.service.CommitService;
 import AISS.GitLabMiner.service.IssueService;
 import AISS.GitLabMiner.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
     @Autowired
-    ProjectService projectService;
-
-    @Autowired
-    RestTemplate restTemplate;
-
+    private RestTemplate restTemplate;
     private final ProjectService project;
     private final CommitService commits;
     private final IssueService issues;
-
     public ProjectController(ProjectService project, CommitService commits, IssueService issues){
         this.project = project;
         this.commits = commits;
         this.issues = issues;
     }
-
-    @GetMapping("/{id}")
+    @GetMapping("/prueba/{id}")
     public Project findAll(@PathVariable String id,
                            @RequestParam(required = false, name = "sinceCommits") Integer sinceCommits,
                            @RequestParam(required = false, name = "sinceIssues") Integer sinceIssues,
                            @RequestParam(required = false, name = "maxPages") Integer maxPages) {
         Project project = this.project.findProject(id);
         project.setCommits(commits.getAllCommits(id,sinceCommits, maxPages));
-        project.setIssue(issues.getAllIssues(id, sinceIssues, maxPages));
+        project.setIssue(issues.getSimpleIssues(id));
 
         return project;
     }
-
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Project postGitMiner(@PathVariable String id,
-                                @RequestParam(required = false, name = "sinceCommits") Integer sinceCommits,
-                                @RequestParam(required = false, name = "sinceIssues") Integer sinceIssues,
-                                @RequestParam(required = false, name = "maxPages") Integer maxPages) {
-        Project proyecto = this.project.findProject(id);
-        proyecto.setCommits(commits.getAllCommits(id,sinceCommits, maxPages));
-        proyecto.setIssue(issues.getAllIssues(id, sinceIssues, maxPages));
+    public Project sendProject(@PathVariable String id,
+                               @RequestParam(required = false, name = "sinceCommits") Integer sinceCommits,
+                               @RequestParam(required = false, name = "sinceIssues") Integer sinceIssues,
+                               @RequestParam(required = false, name = "maxPages") Integer maxPages) {
+        Project project = this.project.findProject(id);
+        project.setCommits(commits.getAllCommits(id,sinceCommits, maxPages));
+        project.setIssue(issues.getSimpleIssues(id));
 
-        Project sentProject = restTemplate.postForObject("http://localhost:8080/api/project", proyecto, Project.class);
-
-        return sentProject;
+        /*
+       restTemplate.postForObject("http://localhost:8081/api/project/" + id
+                + "?sinceCommits=" + sinceCommits + "&sinceIssues="
+                + sinceIssues +"&maxPages=" + maxPages, project, Project.class);
+*/
+        return project;
     }
+
+
 }
+
